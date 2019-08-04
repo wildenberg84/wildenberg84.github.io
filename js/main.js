6a0a38bc94851fae81506408c67a443c602fe01c
Bastiaan Wildenberg <wildenberg84@gmail.com>
2019-08-04 16:25:16 +0200

    local to github

---
+++ js/main.js
@@ -0,0 +1,89 @@
+"use strict"
+
+var searchTermsMap = new Map();
+searchTermsMap.set('github', 'https://github.com/wildenberg84');
+searchTermsMap.set('youtube', 'https://www.youtube.com/channel/UCsQK1Ux9fd5m-iOLlaPjomw?view_as=subscriber');
+
+
+function init(){
+	startTimer();
+	hookSearchBar();
+	hookSearchBtn();
+}
+
+function hookSearchBtn(){
+	var searchBtn = document.querySelector('#go');
+	searchBtn.addEventListener('click', search);
+}
+
+function hookSearchBar(){
+	var searchBar = document.querySelector("#searchBar");
+	searchBar.addEventListener('focus', function(){
+			searchBar.style.width = "30vw";
+		});
+		
+	searchBar.addEventListener('blur', function(){
+			if(searchBar.value == ""){
+				searchBar.style.width = "12vw";
+			}
+		});
+		
+	searchBar.addEventListener('keypress', search);
+}
+
+
+function updateTime(){
+	Date.prototype.getWeek = function() {
+		var onejan = new Date(this.getFullYear(), 0, 1);
+		return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
+	}
+		
+	var now = new Date();
+	var date = now.getDate() + '-' + (now.getMonth()+1) + '-' + now.getFullYear();
+	
+	var seconds = now.getSeconds();
+	var minutes = now.getMinutes();
+	var hours = now.getHours();
+	
+	var time;
+	if(seconds < 10){
+		seconds = "0" + seconds;
+	}
+	if(minutes < 10){
+		minutes = "0" + minutes;
+	}
+	if(hours < 10){
+		hours = "0" + hours;
+	}
+	
+	time = hours + ":" + minutes + ":" + seconds;
+	
+	document.getElementById("date").innerHTML = now.toLocaleDateString();
+	document.getElementById("time").innerHTML = time;
+	
+	var weekNumber = now.getWeek();	
+	document.getElementById("weekNumber").innerHTML = "week " + weekNumber;
+	
+}
+
+function startTimer(){
+	updateTime(); // immediately set display value
+	setInterval(updateTime, 500);
+}
+
+function search(evt){	
+	var term = searchBar.value;
+			
+	if(
+		(
+			(evt.target.id == 'searchBar' && evt.key == 'Enter') || (evt.target.id == 'go' && evt.button == 0)
+		) 
+			&& term != ''){
+		
+		if(!searchTermsMap.has(term.toLowerCase())){			
+			window.open('https://www.google.com/search?q=' + term);
+		}else{
+			window.open(searchTermsMap.get(term));
+		}
+	}
+}
