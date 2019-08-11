e58eb3b8bd5191487b1967aeafd327dd3783a494
Bastiaan Wildenberg <wildenberg84@gmail.com>
2019-08-11 18:03:30 +0200

    added edit form functionality (except for icon)

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 "use strict"
 
+// TODO: refactor code
+
 var searchTermsMap = new Map();
 searchTermsMap.set('github', 'https://github.com/wildenberg84');
 searchTermsMap.set('youtube', 'https://www.youtube.com/channel/UCsQK1Ux9fd5m-iOLlaPjomw?view_as=subscriber');
+searchTermsMap.set('codepen', 'https://codepen.io/wildenberg84/#');
 
 var activeMenuItem;
 
@@ -13,20 +16,109 @@ function init(){
 	hookEditMenu();
 	hookSaveBtn();
 	initCtxMenu();
+	
+	loadQuickLinks();
+}
+
+
+function loadQuickLinks(){
+	if (storageAvailable('localStorage')) {
+		if(localStorage['link1'] == undefined){
+			// link1 should always exist (either set or the default placeholder)
+			initStorageLinks();
+		}else{
+			let link1 = JSON.parse(localStorage['link1']);
+			let link2 = JSON.parse(localStorage['link2']);
+			let link3 = JSON.parse(localStorage['link3']);
+			let link4 = JSON.parse(localStorage['link4']);
+			
+			document.querySelector('#link1').src = link1[0];
+			document.querySelector('#link1').alt = link1[1];
+			document.querySelector('#link1HREF').href = link1[2];
+			
+			// TODO: build links from JS
+			
+			if(link2 != undefined){
+				document.querySelector('#link2').src = link2[0];
+				document.querySelector('#link2').alt = link2[1];
+				document.querySelector('#link2HREF').href = link2[2];				
+			}
+			if(link3 != undefined){
+				document.querySelector('#link3').src = link3[0];
+				document.querySelector('#link3').alt = link3[1];
+				document.querySelector('#link3HREF').href = link3[2];					
+			}
+			if(link4 != undefined){
+				document.querySelector('#link4').src = link4[0];
+				document.querySelector('#link4').alt = link4[1];
+				document.querySelector('#link4HREF').href = link4[2];					
+			}
+		}
+	} else {
+		// no localStorage, use standard HTML
+	}
+}
+
+function initStorageLinks(){
+	let link1 = ['images/github.png', 'GitHub octocat', 'https://github.com/wildenberg84'];
+	let link2 = ['images/codepen.png', 'Codepen pens', 'https://codepen.io/wildenberg84/#'];
+	let link3 = ['images/todo.png', 'todo list', '#'];
+	let link4 = ['images/projects.png', 'projects', '#projects.html'];
+
+	localStorage['link1'] = JSON.stringify(link1);
+	localStorage['link2'] = JSON.stringify(link2);
+	localStorage['link3'] = JSON.stringify(link3);
+	localStorage['link4'] = JSON.stringify(link4);
+}
+
+function storageAvailable(type) {
+    var storage;
+    try {
+        storage = window[type];
+        var x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
 }
 
 function hookSaveBtn(){
-	document.querySelector('#saveBtn').addEventListener('click', save(activeMenuItem));
+	document.querySelector('#saveBtn').addEventListener('click', save);
 }
 
-function save(item){
-	// set item to new values
-	// save to localStorage
-		// warn in case of error
+function save(){
+	try{
+		localStorage[activeMenuItem.id] = JSON.stringify([document.querySelector('#linkIcon').src, document.querySelector('#descr').value, document.querySelector('#url').value]);
+		
+		activeMenuItem.src = document.querySelector('#linkIcon').src;
+		activeMenuItem.alt = document.querySelector('#descr').value;
+		document.querySelector('#' + activeMenuItem.id + 'HREF').href = document.querySelector('#url').value;
+	} catch(error){
+		alert(error);
+	}
+	
+	activeMenuItem = undefined;
+	document.querySelector('#editMenuItem').style.display = 'none';
 }
 
 function hookEditMenu(){
 	document.querySelector('#close').addEventListener('click', function(evt){
+		activeMenuItem = undefined;
+			
 		var editMenu = document.querySelector('#editMenuItem');
 		editMenu.style.display = 'none';
 	});
@@ -60,7 +152,6 @@ function showCtxMenu(event){
 }
 
 function hideCtxMenu(){
-	activeMenuItem = undefined;
 	ctxMenu.style.display = 'none';
 }
 
@@ -75,11 +166,13 @@ function processClick(event){
 				document.querySelector('#url').value = document.querySelector('#' + activeMenuItem.id + 'HREF').href;
 				document.querySelector('#linkIcon').src = activeMenuItem.src;
 				document.querySelector('#editMenuItem').style.display = 'flex';
+				
 				hideCtxMenu();
 				break;
 			case 'remove': 
 				activeMenuItem.style.display = 'none';
-				// remove from localStorage!!!
+				// TODO: remove from localStorage
+				// TODO: if it's the last item, add a empty placeholder
 				hideCtxMenu();
 				break;
 			default: break;
