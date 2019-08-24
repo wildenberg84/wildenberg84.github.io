e9ba2facae0e30db77ee7d94aa906da979bb91fe
Bastiaan Wildenberg <wildenberg84@gmail.com>
2019-08-24 23:44:07 +0200

    fixed several (link) storage issues and altered working of empty links

--- js/main.js
+++ js/main.js
@@ -39,16 +39,8 @@ function hookIconUpload(){
 				alert('Invalid image type!');
 				return false;
 			}else{			
-				// load image to src
-				const reader = new FileReader();
-		  
-				reader.onload = (function(aImg) { 
-					return function(e) { 
-						aImg.src = e.target.result; 
-					}; 
-				})(img);
-		  
-				img.src = reader.readAsDataURL(file);
+				// asynch load image
+				getImgFile(img, file);
 			}
 		}
 	);
@@ -63,23 +55,28 @@ function hookIconUpload(){
 	  });
 
 	img.addEventListener('drop', function(evt) {
-	  evt.stopPropagation();
-	  evt.preventDefault();
-	  var files = evt.dataTransfer.files;  // FileList object.
-	  var file = files[0];                // File     object.
+		evt.stopPropagation();
+		evt.preventDefault();
+		var files = evt.dataTransfer.files;  // FileList object.
+		var file = files[0];                // File     object.
 		
-	  const reader = new FileReader();
-	  
-	  reader.onload = (function(aImg) { 
-		return function(e) { 
-		  aImg.src = e.target.result; 
-		}; 
-	  })(img);
-	  
-	  // NOTE there is absolutely no check if the dropped file is actually an image
-	  img.src = reader.readAsDataURL(file);
+		// asynch load image
+		getImgFile(img, file);
 	});
 }
+
+const toBase64 = file => new Promise((resolve, reject) => {
+	const reader = new FileReader();
+	reader.readAsDataURL(file);
+	reader.onload = () => resolve(reader.result);
+	reader.onerror = error => reject(error);
+});
+
+async function getImgFile(imgElem, file) {
+	// NOTE storage is limited, it's highly adviced to check the file's size and other limitations
+	imgElem.src = await toBase64(file);;
+}
+
 
 // type parameter is full file.type
 function checkValidImageType(type){
@@ -96,51 +93,86 @@ function checkValidImageType(type){
 function loadQuickLinks(){
 	if (storageAvailable('localStorage')) {
 		if(localStorage['link1'] == undefined){
-			// link1 should always exist (either set or the default placeholder)
-			initStorageLinks();
+			// no links to load
 		}else{
-			let link1 = JSON.parse(localStorage['link1']);
-			let link2 = JSON.parse(localStorage['link2']);
-			let link3 = JSON.parse(localStorage['link3']);
-			let link4 = JSON.parse(localStorage['link4']);
+			let link1 = localStorage['link1'];
+			let link2 = localStorage['link2'];
+			let link3 = localStorage['link3'];
+			let link4 = localStorage['link4'];
 			
-			document.querySelector('#link1').src = link1[0];
-			document.querySelector('#link1').alt = link1[1];
-			document.querySelector('#link1HREF').href = link1[2];
 			
-			// TODO: build links from JS
+			if(link1 != undefined){
+				link1 = JSON.parse(link1); // either check the storage or catch SyntaxError from JSON
+				document.querySelector('#link1').src = link1[0];
+				if(document.querySelector('#link1').src = './images/noicon.png'){
+					document.querySelector('#link1').classList.toggle('empty');
+				}
+				
+				document.querySelector('#link1').alt = link1[1];
+				document.querySelector('#link1HREF').href = link1[2];
+			
+				if(link1[3] == true){
+					document.querySelector('#link1HREF').target = '_blank';
+				}else{
+					document.querySelector('#link1HREF').target = '_self';	
+				}
+			}
 			
 			if(link2 != undefined){
+				link2 = JSON.parse(link2);
+				
 				document.querySelector('#link2').src = link2[0];
+				if(document.querySelector('#link2').src = './images/noicon.png'){
+					document.querySelector('#link2').classList.toggle('empty');
+				}
+				
 				document.querySelector('#link2').alt = link2[1];
-				document.querySelector('#link2HREF').href = link2[2];				
+				document.querySelector('#link2HREF').href = link2[2];
+				
+				if(link2[3] == true){
+					document.querySelector('#link2HREF').target = '_blank';				
+				}else{
+					document.querySelector('#link2HREF').target = '_self';	
+				}
 			}
 			if(link3 != undefined){
+				link3 = JSON.parse(link3);
+				
 				document.querySelector('#link3').src = link3[0];
+				if(document.querySelector('#link3').src = './images/noicon.png'){
+					document.querySelector('#link3').classList.toggle('empty');
+				}
+				
 				document.querySelector('#link3').alt = link3[1];
-				document.querySelector('#link3HREF').href = link3[2];					
+				document.querySelector('#link3HREF').href = link3[2];
+
+				if(link3[3] == true){
+					document.querySelector('#link3HREF').target = '_blank';				
+				}else{
+					document.querySelector('#link3HREF').target = '_self';	
+				}
 			}
 			if(link4 != undefined){
+				link4 = JSON.parse(link4);
+				
 				document.querySelector('#link4').src = link4[0];
+				if(document.querySelector('#link4').src = './images/noicon.png'){
+					document.querySelector('#link4').classList.toggle('empty');
+				}
+				
 				document.querySelector('#link4').alt = link4[1];
-				document.querySelector('#link4HREF').href = link4[2];					
+				document.querySelector('#link4HREF').href = link4[2];
+				
+				if(link4[3] == true){
+					document.querySelector('#link4HREF').target = '_blank';				
+				}else{
+					document.querySelector('#link4HREF').target = '_self';	
+				}
 			}
 		}
 	} else {
-		// no localStorage, use standard HTML
+		document.querySelector('#quickLinks').style.display = 'none';
 	}
-}
-
-function initStorageLinks(){
-	var link1 = ['images/github.png', 'GitHub octocat', 'https://github.com/wildenberg84'];
-	var link2 = ['images/codepen.png', 'Codepen pens', 'https://codepen.io/wildenberg84/#'];
-	var link3 = ['images/todo.png', 'todo list', '#'];
-	var link4 = ['images/projects.png', 'projects', '#projects.html'];
-
-	localStorage['link1'] = JSON.stringify(link1);
-	localStorage['link2'] = JSON.stringify(link2);
-	localStorage['link3'] = JSON.stringify(link3);
-	localStorage['link4'] = JSON.stringify(link4);
 }
 
 // from https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
@@ -175,11 +207,26 @@ function hookSaveBtn(){
 
 function save(){
 	try{
-		localStorage[activeMenuItem.id] = JSON.stringify([document.querySelector('#linkIcon').src, document.querySelector('#descr').value, document.querySelector('#url').value]);
+		var iconImg = undefined;
 		
-		activeMenuItem.src = document.querySelector('#linkIcon').src;
+		if(activeMenuItem.classList.contains('empty')){
+			activeMenuItem.classList.toggle('empty');
+			iconImg = './images/noicon.png';
+		}else{
+			iconImg = document.querySelector('#linkIcon').src;
+		}
+		
+		localStorage[activeMenuItem.id] = JSON.stringify([iconImg, document.querySelector('#descr').value, document.querySelector('#url').value, document.querySelector('#newTab').checked]);
+		
+		activeMenuItem.src = iconImg;
 		activeMenuItem.alt = document.querySelector('#descr').value;
 		document.querySelector('#' + activeMenuItem.id + 'HREF').href = document.querySelector('#url').value;
+		
+		if(document.querySelector('#newTab').checked){
+			document.querySelector('#' + activeMenuItem.id + 'HREF').target = '_blank';
+		}else{
+			document.querySelector('#' + activeMenuItem.id + 'HREF').target = '';
+		}
 	} catch(error){
 		alert(error);
 	}
@@ -187,6 +234,7 @@ function save(){
 	activeMenuItem = undefined;
 	document.querySelector('#editMenuItem').style.display = 'none';
 }
+
 
 function hookEditMenu(){
 	document.querySelector('#close').addEventListener('click', function(evt){
@@ -224,28 +272,47 @@ function showCtxMenu(event){
 	event.preventDefault(); // disable the default behavior of the contextmenu event
 }
 
+function showCtxMenu(){};
+
 function hideCtxMenu(){
 	ctxMenu.style.display = 'none';
 }
 
 function processClick(event){
+	if(event.target.classList.contains('link') && event.target.classList.contains('empty')){
+		// simulate contextmenu event
+		alert('implement contextmenu event');
+	}
+	
 	if(event.target.classList.contains('menuItem')){
 		var task = event.target.dataset.value;
 		
 		// Menu item edit
 		switch(task){
 			case 'edit': 
-				document.querySelector('#descr').value = activeMenuItem.alt;
-				document.querySelector('#url').value = document.querySelector('#' + activeMenuItem.id + 'HREF').href;
-				document.querySelector('#linkIcon').src = activeMenuItem.src;
+				document.querySelector('#descr').value = activeMenuItem.getAttribute('alt');
+				
+				if(document.querySelector('#' + activeMenuItem.id + 'HREF').getAttribute('href') == ''){
+					document.querySelector('#url').value = 'https://';
+				}else{
+					document.querySelector('#url').value = document.querySelector('#' + activeMenuItem.id + 'HREF').getAttribute('href');
+				}
+					
+				document.querySelector('#linkIcon').src = activeMenuItem.getAttribute('src');
+				document.querySelector('#newTab').checked = document.querySelector('#' + activeMenuItem.id + 'HREF').checked;
 				document.querySelector('#editMenuItem').style.display = 'flex';
 				
 				hideCtxMenu();
 				break;
 			case 'remove': 
-				activeMenuItem.style.display = 'none';
-				// TODO: remove from localStorage
-				// TODO: if it's the last item, add an empty placeholder
+				localStorage.removeItem(activeMenuItem.id);
+				activeMenuItem.classList.toggle('empty');
+				
+				document.querySelector('#' + activeMenuItem.id).src = 'images/addlink.png';
+				document.querySelector('#' + activeMenuItem.id).alt = '';
+				document.querySelector('#' + activeMenuItem.id + 'HREF').href = '';
+				document.querySelector('#' + activeMenuItem.id + 'HREF').target = '';	
+				
 				hideCtxMenu();
 				break;
 			default: break;
